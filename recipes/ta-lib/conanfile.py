46539c42b92ff57c1bbd0970c81c1ebf2753d138
Curtis Lo <sheepgrass@gmail.com>
2022-01-22 09:50:21 +0800

    add conan recipe for ta-lib

---
+++ recipes/ta-lib/conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, CMake, tools
+
+
+class TaLibConan(ConanFile):
+    name = "ta-lib"
+    license = "TA-Lib BSD License"
+    homepage = "https://sourceforge.net/projects/ta-lib/"
+    url = "http://localhost:9300/"  # Package recipe repository url here, for issues about the package
+    description = "Technical analysis library with indicators like ADX, MACD, RSI, Stochastic, TRIX..."
+    topics = ("conan", "technical analysis", "indicator")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+
+    def source(self):
+        if self.settings.os == "Windows":
+            tools.get(**self.conan_data["sources"][self.version]["msvc"])
+        else:
+            tools.get(**self.conan_data["sources"][self.version]["src"])
+
+    def build(self):
+        pass
+
+    def package(self):
+        if self.settings.os == "Windows":
+            self.copy("LICENSE*", dst="licenses", src="ta-lib")
+            self.copy("*.h", dst="include/%s" % self.name, src="ta-lib/c/include")
+            self.copy("*.lib", dst="lib", src="ta-lib/c/lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
---
+++ recipes/ta-lib/test_package/example.cpp
@@ -0,0 +1,6 @@
+#include "ta-lib/ta_libc.h"
+
+int main()
+{
+    return 0;
+}
