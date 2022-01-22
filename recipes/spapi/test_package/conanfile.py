47516e8e3034cf50fd497b2ae322e61e16bb4be6
Curtis Lo <sheepgrass@gmail.com>
2022-01-22 09:50:30 +0800

    correct test package class name in spapi recipe

--- recipes/spapi/test_package/conanfile.py
+++ recipes/spapi/test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-class TaLibTestConan(ConanFile):
+class SpapiTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
